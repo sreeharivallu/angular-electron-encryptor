498bf765e8cb0be0472876b91e41dd78e3b57c6a
Sreehari  Vallu <241600@TCS.com>
2019-01-22 19:20:52 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-electron-encyptor';
+  encrypted_result = '';
+  decrypted_result= '';
+  iv='';
+
+constructor(){
+  this.iv = CryptoJS.enc.Utf8.parse('7061737323313233');
 }
+  
+encrypt(secret:any, mystring:any){
+  console.log('secret is', secret.value);
+  var key = CryptoJS.enc.Utf8.parse(secret.value);
+  
+  this.encrypted_result = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(mystring.value), key,
+  {
+      keySize: 128 / 8,
+      iv: this.iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+  });
+}
+
+decrypt(secret:any, encrypted:any){
+  var key = CryptoJS.enc.Utf8.parse(secret.value);  
+  var result = CryptoJS.AES.decrypt(encrypted.value, key, {
+    keySize: 128 / 8,
+    iv: this.iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  });
+
+  this.decrypted_result = result.toString(CryptoJS.enc.Utf8);
+}
+}
+
+
